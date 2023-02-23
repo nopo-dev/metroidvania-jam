d8aa6d8ba58ef432dc7418f0f6a39a06df89b0dd
Leon Xu <leonxu@berkeley.edu>
2023-02-23 14:20:37 -0800

    Add script Utils.

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,10 @@
+/*
+ * A collection of public utils
+ */
+public static class Utils
+{
+    public static int Clamp(int value, int min, int max)
+    {
+        return (value < min) ? min : (value > max) ? max : value;
+    }
+}
