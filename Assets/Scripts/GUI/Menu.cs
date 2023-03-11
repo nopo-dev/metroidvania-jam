f8cdea32be6398cb82b46f74550a9d72f0ea5a20
Brian Fang <brianfang@ucla.edu>
2023-03-11 00:56:13 -0800

    basic main menu

---
+++ Assets/Scripts/GUI/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Location _nextSceneSpawnPoint;
+
+    //TODO: change so that location is pulled from save data 
+    public void PlayButton()
+    {
+        SceneLoader.Instance.loadScene(_nextSceneSpawnPoint);
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
