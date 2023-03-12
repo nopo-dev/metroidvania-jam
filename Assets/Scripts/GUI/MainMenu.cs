f2f004425c4a38048d4b01213d1b841d1299875d
Brian Fang <51388781+b-fang@users.noreply.github.com>
2023-03-11 20:23:56 -0800

    Pause menu (#52)
    * basic main menu
    
    * basic functioning pause menu
    
    * quit to title button
    
    * new temp gui prefab

---
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Location _nextSceneSpawnPoint;
+
+    public static MainMenu Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.Log("Can only have one MainMenu!");
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //TODO: change so that location is pulled from save data 
+    public void PlayButton()
+    {
+        SceneLoader.Instance.loadScene(_nextSceneSpawnPoint);
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+    public bool getActive()
+    {
+        return gameObject.activeSelf;
+    }
+    //TODO: add another button that says resume rather than play if savedata exists
+
+}
--- Assets/Scripts/GameControl/PauseControl.cs
+++ Assets/Scripts/GameControl/PauseControl.cs
@@ -1,8 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public static class PauseControl
+public class PauseControl : MonoBehaviour
 {
     public static bool gameIsPaused = false;
+
+    void Update()
+    {
+        //TODO: check if not on main menu / other checks to make sure you are allowed to pause
+        if (Input.GetKeyDown(KeyCode.Escape) && !MainMenu.Instance.getActive())
+        {
+            if (PauseControl.gameIsPaused)
+            {
+                PauseMenu.Instance.Resume();
+            }
+            else
+            {
+                PauseMenu.Instance.Pause();
+            }
+        }
+    }
 
     public static void PauseGame()
     {
